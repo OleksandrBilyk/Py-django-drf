53cea7e63c3ff616c375dcd84599de2930a52831
Oleksandr <biluksanya@ukr.net>
2024-06-08 11:01:46 +0300

    lesson7_part2

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,8 @@
+from core.enums.regex_enum import RegexEnum
 from core.models import BaseModel
 from core.services.upload_avatar import upload_avatar
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core import validators as V
 from django.db import models
 
 from apps.users.managers import UserManager
@@ -11,7 +13,7 @@ class UserModel(AbstractBaseUser, PermissionsMixin, BaseModel):
         db_table = 'auth_users'
 
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=128, validators=[V.RegexValidator(*RegexEnum.PASSWORD.value)])
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
@@ -24,9 +26,14 @@ class ProfileModel(BaseModel):
     class Meta:
         db_table = 'profile'
 
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    age = models.IntegerField()
+    name = models.CharField(max_length=20, validators=[
+        V.RegexValidator(*RegexEnum.NAME.value)
+    ])
+    surname = models.CharField(max_length=20, validators=[
+        V.RegexValidator(*RegexEnum.NAME.value)
+    ])
+    age = models.IntegerField(validators=[V.MinValueValidator(15), V.MaxValueValidator(130)])
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name='profile')
-    avatar = models.ImageField(upload_to=upload_avatar, blank=True)
+    avatar = models.ImageField(upload_to=upload_avatar, blank=True, validators=(V.FileExtensionValidator(['gif', 'jpeg', 'png']),))
+
     
